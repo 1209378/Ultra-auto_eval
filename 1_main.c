34b2baa89b7b7fb1313a8c59d358e158202b9e2e
1209378 <15183116485@163.com>
2025-10-23 00:16:00 +0800

    Update main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,28 @@
-//
-//
-//
+//202516301209
+//15183116485@163.com
+//廖清惠
 #include <stdio.h>
 int main()
 {
     printf("Hello World");
     return 0;
+}
+#include <stdio.h>  
+
+int main() {
+    
+    float celsius, fahrenheit;
+
+   
+    printf("请输入摄氏温度：");
+   
+    scanf("%f", &celsius);
+
+   
+    fahrenheit = celsius * 9 / 5 + 32;
+
+    
+    printf("转换后的华氏温度为：%.1f\n", fahrenheit);
+
+    return 0;  
 }
